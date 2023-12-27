a1a2e0bbb3fc363f6c078e821e6878532dd90a97
champark <97137143+champark@users.noreply.github.com>
2023-12-28 00:02:15 +0900

    program completed
    틀린그림찾기 프로그램 완성.
    자동클릭기능 추가

--- FindDifference/find_diff.py
+++ FindDifference/find_diff.py
@@ -1,5 +1,6 @@
 import os, time
 import pyautogui
+import cv2
 from PIL import ImageChops
 
 # 원본 이미지
@@ -12,42 +13,52 @@ from PIL import ImageChops
 # width 847
 # height 678
 
-width, height = 865, 693
-x_pos = 1030
 
-src = pyautogui.screenshot(region=(x_pos, 64, width, height))
-src.save('src.jpg')
+while True:
+    width, height = 865, 693
+    x_pos = 1076
 
-dest = pyautogui.screenshot(region=(x_pos, 761, width, height))
-dest.save('dest.jpg')
+    result = pyautogui.confirm('틀린 그림 찾기', buttons=['시작','종료'])
+    if result == '종료':
+        break
+    src = pyautogui.screenshot(region=(x_pos, 64, width, height))
+    # src.save('src.jpg')
 
-diff = ImageChops.difference(src, dest)
-diff.save('diff.jpg')
+    dest = pyautogui.screenshot(region=(x_pos, 761, width, height))
+    # dest.save('dest.jpg')
 
-# 파일 생성 대기
-while not os.path.exists('diff.jpg'):
-    time.sleep(1)
+    diff = ImageChops.difference(src, dest)
+    diff.save('diff.jpg')
 
-import cv2
-src_img = cv2.imread('src.jpg')
-dest_img = cv2.imread('dest.jpg')
-diff_img = cv2.imread('diff.jpg')
+    # 파일 생성 대기
+    while not os.path.exists('diff.jpg'):
+        time.sleep(1)
 
-gray = cv2.cvtColor(diff_img, cv2.COLOR_BGR2GRAY)
-gray = (gray > 25) * gray
-contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # src_img = cv2.imread('src.jpg')
+    # dest_img = cv2.imread('dest.jpg')
+    diff_img = cv2.imread('diff.jpg')
 
-COLOR = (0, 200, 0)
-for cnt in contours:
-    if cv2.contourArea(cnt) > 100:
-        x, y, width, height = cv2.boundingRect(cnt)
-        cv2.rectangle(src_img, (x, y), (x + width, y + height), COLOR, 2)
-        cv2.rectangle(dest_img, (x, y), (x + width, y + height), COLOR, 2)
-        cv2.rectangle(diff_img, (x, y), (x + width, y + height), COLOR, 2)
+    gray = cv2.cvtColor(diff_img, cv2.COLOR_BGR2GRAY)
+    gray = (gray > 25) * gray
+    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-cv2.imshow('src', src_img)
-cv2.imshow('dest', dest_img)
-cv2.imshow('diff', diff_img)
+    COLOR = (0, 200, 0)
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 100:
+            x, y, width, height = cv2.boundingRect(cnt)
+            # cv2.rectangle(src_img, (x, y), (x + width, y + height), COLOR, 2)
+            # cv2.rectangle(dest_img, (x, y), (x + width, y + height), COLOR, 2)
+            # cv2.rectangle(diff_img, (x, y), (x + width, y + height), COLOR, 2)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+            to_x = x + (width //2 ) + x_pos
+            to_y = y + (height //2 ) + 64
+            pyautogui.moveTo(to_x, to_y, duration= 0.15)
+            #time.sleep(1)
+            pyautogui.click(to_x, to_y)
+
+    # cv2.imshow('src', src_img)
+    # cv2.imshow('dest', dest_img)
+    # cv2.imshow('diff', diff_img)
+
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
